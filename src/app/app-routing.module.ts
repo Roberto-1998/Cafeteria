4c825051cd9228d106028655917ae7d7c1065ce5
Roberto <rcastellong98@gmail.com>
2021-11-10 19:55:17 +0100

    Integración del Template al Proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'admin', component:LoginComponent},
+  {path:'',
+     component:PagesComponent,
+     children:[
+      {path:'home',component:HomeComponent },
+      {path:'menu', component:MenuComponent},
+      {path:'contact', component:ContactComponent},
+      {path:'about', component:AboutComponent},
+      {path:'gallery', component:GalleryComponent},
+      {path:'reservation', component:ReservationComponent},
+      {path:'', redirectTo:'home', pathMatch:'full'}
+    ]},
+  {path:'**', component:NotFoundComponent, pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { PagesComponent } from './pages/pages.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    MenuComponent,
+    ReservationComponent,
+    GalleryComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    AboutComponent,
+    NotFoundComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
